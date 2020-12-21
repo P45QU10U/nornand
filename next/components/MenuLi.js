baf6cbe949a2a7bdd1ef2c4bb027d754f771068f
Richard Pasquiou <services@pasquiou.fr>
2020-12-21 22:53:34 +0100

    ajouté items li

---
+++ next/components/MenuLi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function MenuLi({children}) {
+
+  return (
+    <li className="text-center">
+      {children}
+    </li>
+  )
+}
--- next/components/menu.js
+++ next/components/menu.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import MenuLi from './MenuLi'
 
 export default function Menu() {
 
   const nomDeLaBoite = 'Nornand'
 
-  const [toggle, setToggle] = React.useState(false)
+  // const [toggle, setToggle] = React.useState(false)
+  const [toggle, setToggle] = React.useState(true)
 
   const toggleMenu = (e) => {
     e.preventDefault()
@@ -15,30 +17,30 @@ export default function Menu() {
   const displayMenu = toggle ? 'hidden' : ''
 
   return (
-    <nav className="bg-blue-300  w-full ">
-      <div className="flex align-items justify-between">
+    <nav className="p-4 bg-blue-300  w-full">
+      <div className="flex align-items justify-between mb-6">
         <h2 className="">
           <Link href="/">
             {nomDeLaBoite}
           </Link>
         </h2>
-        <button className="block md:hidden" type="button" onClick={toggleMenu} aria-expanded={toggle} aria-controls="menu">
-          <svg className="h-6 w-6" id="i-menu" aria-label="Menu" viewBox="0 0 32 32" width="16" height="16" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%">
+        <button className="block md:hidden px-4" type="button" onClick={toggleMenu} aria-expanded={toggle} aria-controls="menu">
+          <svg className="h-8 w-8 fill-current" id="i-menu" aria-label="Menu" viewBox="0 0 32 32" width="16" height="16" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%">
             <path d="M4 8 L28 8 M4 16 L28 16 M4 24 L28 24"></path>
           </svg>
         </button>
       </div>
-      <ul id="menu" className="md:flex md:flex-wrap md:flex-row md:items-center justify-center md:justify-end " hidden={!toggle}>
-        <li className="p-4">
-          <a href="/">Home</a>
-        </li>
-        <li className="p-4">
+      <ul id="menu" className="bg-gray-400 flex flex-col align-items justify-center md:flex md:flex-wrap md:flex-row md:items-center md:justify-end" hidden={!toggle}>
+        <MenuLi>
+          <Link href="/">Home</Link>
+        </MenuLi>
+        <li className="">
           <a href="/benefits">Benefits</a>
         </li>
-        <li className="p-4">
+        <li className="">
           <a href="/pricing">Pricing</a>
         </li>
-        <li className="p-4">
+        <li className="">
           <a href="/blog">Blog</a>
         </li>
       </ul>
