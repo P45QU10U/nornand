f3d63e4faefead07d6b1c5225e9f5e67c16dfc64
Richard Pasquiou <services@pasquiou.fr>
2021-01-11 15:33:08 +0100

    load async snipcart script

--- next/components/ecommerce/snipcartBottom.js
+++ next/components/ecommerce/snipcartBottom.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { snipcartlib } from '../../../config'
 import useScript from '../../hooks/useScript'
 
-const snipcartApiKey = 'YOUR_PUBLIC_API_KEY'
+const snipcartApiKey = process.env.NEXT_PUBLIC_SNIPCART_API_KEY
 
 export default function SnipcartBottom({ commerce = false }) {
   const status = useScript(snipcartlib)
 
-  return status === 'ready' && commerce && <div hidden id="snipcart" data-api-key={snipcartApiKey} />
+  return (
+    (status === 'loading' || status === 'ready') &&
+    commerce && <div hidden id="snipcart" data-api-key={snipcartApiKey} />
+  )
 }
